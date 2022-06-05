73afb84917bf2d9fc504d7f391268ba761fd6eae
Salvatore Torcivia <github+0019@saltor.nyc>
2022-06-05 15:47:28 -0400

    incorporate date-fns into solution

--- index.js
+++ index.js
@@ -2,31 +2,59 @@
 // where your node app starts
 
 // init project
-var express = require('express');
-var app = express();
+var express = require('express')
+var app = express()
+
+const { isValid, format, toDate, parse, parseISO } = require('date-fns')
+const { enUS } = require('date-fns/locale')
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
-var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+// so that your API is remotely testable by FCC
+var cors = require('cors')
+app.use(cors({ optionsSuccessStatus: 200 })) // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
+app.use(express.static('public'))
 
 // http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
-});
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/views/index.html')
+})
 
+// your first API endpoint...
+app.get('/api/hello', function (req, res) {
+  res.json({ greeting: 'hello API' })
+})
 
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
-});
+app.get('/api/:date', function (req, res) {
+  const { date } = req.params
+  const isDateStr = /^\d{4}-\d{2}-\d{2}$/.test(date)
+  let converted
 
+  try {
+    if (isDateStr) {
+      converted = toDate(parse(date, 'yyyy-MM-dd', new Date()))
+    } else if (/^\d+$/.test(date)) {
+      converted = toDate(parseInt(date, 10))
+    }
+  } catch (error) {
+    console.log(error)
+  }
 
+  if (!isValid(converted)) {
+    return res.json({ error: 'Invalid Date' })
+  }
+
+  return res.json({
+    unix: converted.getTime(),
+    utc: format(
+      parseISO(converted.toISOString()),
+      'iii, d MMM yyyy HH:mm:ss'
+    ).concat(' GMT'),
+  })
+})
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
-});
+var listener = app.listen('12345', function () {
+  console.log('Your app is listening on port ' + listener.address().port)
+})
